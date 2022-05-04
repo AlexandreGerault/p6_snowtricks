af5420c286e155991f41ea3a0f719bb9cf5fcbee
Alexandre Gérault <alexandre@hexium.io>
2022-05-04 20:18:38 +0200

    feat(security): user entity

---
+++ apps/symfony/src/Security/Entity/User.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Security\Entity;
+
+use App\Security\Repository\UserRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Uid\Uuid;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ORM\Table(name: '`users`')]
+#[UniqueEntity('email')]
+class User
+{
+    #[ORM\Id]
+    #[ORM\Column(type: "uuid", unique: true)]
+    #[ORM\GeneratedValue(strategy: "CUSTOM")]
+    #[ORM\CustomIdGenerator(class: 'doctrine.uuid_generator')]
+    private Uuid $uuid;
+
+    #[ORM\Column(type: Types::STRING, unique: true)]
+    private string $username;
+
+    #[ORM\Column(type: Types::STRING, unique: true)]
+    private string $email;
+
+    #[ORM\Column(type: Types::STRING, unique: true)]
+    private string $password;
+
+    public function username(): string
+    {
+        return $this->username;
+    }
+
+    public function email(): string
+    {
+        return $this->email;
+    }
+
+    public function setUsername(string $username): static
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function setEmail(string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function setPassword(string $password): static
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+}
