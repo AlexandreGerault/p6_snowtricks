28b227d17f8afd453a866015a0c2852b333fb1c4
Alexandre Gérault <alexandre@hexium.io>
2022-05-04 20:19:14 +0200

    feat(security): generate admin user in fixtures

---
+++ apps/symfony/src/Security/DataFixtures/UserFixture.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Security\DataFixtures;
+
+use App\Security\Entity\UserFactory;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class UserFixture extends Fixture
+{
+    public const ADMIN_NAME = 'Admin';
+    public const ADMIN_MAIL = 'admin@snowtricks.fr';
+
+    public const PASSWORD = 'password';
+
+    public function __construct(private readonly UserFactory $userFactory)
+    {
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $admin = $this->userFactory->create(self::ADMIN_NAME, self::ADMIN_MAIL, self::PASSWORD);
+
+        $manager->persist($admin);
+
+        $manager->flush();
+    }
+}
