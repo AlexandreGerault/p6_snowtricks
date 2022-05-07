53d9253609825616692f15914c9ff6a0ba19f097
Alexandre Gérault <gerault-alexandre@orange.fr>
2022-05-07 09:35:55 +0200

    feat(security): include real confirm link in mail

--- apps/symfony/src/Security/Register/RegisterController.php
+++ apps/symfony/src/Security/Register/RegisterController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Security\Register;
 
+use App\Security\Entity\ActivationToken;
 use App\Security\Entity\UserFactory;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -13,13 +14,14 @@ use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class RegisterController extends AbstractController
 {
     public function __construct(
         private readonly UserFactory $factory,
         private readonly EntityManagerInterface $entityManager,
-        private readonly MailerInterface $mailer,
+        private readonly MailerInterface $mailer
     ) {
     }
 
@@ -38,10 +40,19 @@ class RegisterController extends AbstractController
 
             $user = $this->factory->create($data->username, $data->email, $data->password);
             $user->setPassword($hasher->hashPassword($user, $data->password));
+
+            $activationToken = new ActivationToken();
+            $activationToken->setToken("token");
+            $activationToken->setUser($user);
+
             $this->entityManager->persist($user);
+            $this->entityManager->persist($activationToken);
             $this->entityManager->flush();
 
-            $this->sendConfirmationLink($user->email());
+            $this->sendConfirmationLink(
+                $user->email(),
+                $this->generateUrl('app_activate_account', ['token' => $activationToken->getToken()], UrlGeneratorInterface::ABSOLUTE_URL)
+            );
 
             return $this->redirect('/inscription');
         }
@@ -52,9 +63,9 @@ class RegisterController extends AbstractController
     /**
      * @throws TransportExceptionInterface
      */
-    private function sendConfirmationLink(string $to): void
+    private function sendConfirmationLink(string $to, string $confirmLink): void
     {
-        $mail = new RegisterConfirmationLinkMail("hlhl", $to);
+        $mail = new RegisterConfirmationLinkMail($confirmLink, $to);
         $this->mailer->send($mail);
     }
 }
--- apps/symfony/tests/Web/Security/RegisterTest.php
+++ apps/symfony/tests/Web/Security/RegisterTest.php
@@ -5,14 +5,17 @@ declare(strict_types=1);
 namespace App\Tests\Web\Security;
 
 use App\Security\DataFixtures\UserFixture;
+use App\Security\Entity\ActivationToken;
 use App\Security\Entity\User;
 use App\Security\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class RegisterTest extends WebTestCase
 {
-    public function test_it_can_register_a_user_and_send_a_confirmation_email(): void
+    public function test_it_can_register_a_user_and_send_a_confirmation_email_with_activation_token(): void
     {
         $client = static::createClient();
 
@@ -35,7 +38,21 @@ class RegisterTest extends WebTestCase
         $this->assertFalse($createdUser->isActive());
         $this->assertNotEquals("password", $createdUser->getPassword()); // check password has been hashed
 
+        /** @var EntityManagerInterface $em */
+        $em = $this->getContainer()->get(EntityManagerInterface::class);
+        $token = $em->createQueryBuilder()
+            ->select('a')
+            ->from(ActivationToken::class, 'a')
+            ->innerJoin('a.user', 'u')
+            ->where('u.uuid = :id')
+            ->setParameter('id', $createdUser->id()->toBinary())
+            ->getQuery()
+            ->getOneOrNullResult();
+        $this->assertInstanceOf(ActivationToken::class, $token);
+
+        $message = $this->getMailerMessage();
         $this->assertEmailCount(1);
+        $this->assertEmailHtmlBodyContains($message, "/confirmer?token={$token->getToken()}");
     }
 
     public function test_it_cannot_register_a_user_with_an_email_already_in_use(): void
@@ -53,6 +70,9 @@ class RegisterTest extends WebTestCase
         ]);
         $this->assertResponseIsSuccessful();
 
-        $this->assertStringContainsString(UserFixture::ADMIN_MAIL . " est déjà utilisé comme adresse mail.", $crawler->html());
+        $this->assertStringContainsString(
+            UserFixture::ADMIN_MAIL . " est déjà utilisé comme adresse mail.",
+            $crawler->html()
+        );
     }
 }
