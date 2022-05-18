248698ae2e07dce814d108f64bd62970362d8a72
Alexandre Gérault <gerault-alexandre@orange.fr>
2022-05-18 19:13:35 +0200

    fix(security): hash password when loading fixtures

--- apps/symfony/src/Security/Entity/UserFactory.php
+++ apps/symfony/src/Security/Entity/UserFactory.php
@@ -4,13 +4,21 @@ declare(strict_types=1);
 
 namespace App\Security\Entity;
 
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
 class UserFactory
 {
+    public function __construct(private UserPasswordHasherInterface $hasher)
+    {
+
+    }
+
     public function create(string $username, string $email, string $password): User
     {
-        return (new User())
-            ->setUsername($username)
-            ->setEmail($email)
-            ->setPassword($password);
+        $user = new User();
+        $user->setUsername($username);
+        $user->setEmail($email);
+        $user->setPassword($this->hasher->hashPassword($user, $password));
+        return $user;
     }
 }
