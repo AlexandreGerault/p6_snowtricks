868a3e5097ff19284e0ec0ef12723c6494163ff5
Alexandre Gérault <gerault-alexandre@orange.fr>
2022-05-29 12:32:00 +0200

    test: add helper to fetch user

---
+++ apps/symfony/tests/Helpers/Security/FetchUser.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Helpers\Security;
+
+use App\Security\Entity\User;
+use App\Security\Repository\UserRepository;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class FetchUser
+{
+    private function __construct(private readonly ContainerInterface $container)
+    {
+    }
+
+    public static function new(ContainerInterface $container): static
+    {
+        return new self($container);
+    }
+
+    public function fetchUserByMail(string $email): User
+    {
+        $userRepository = $this->container->get(UserRepository::class);
+
+        return $userRepository->findOneBy(['email' => $email]);
+    }
+}
