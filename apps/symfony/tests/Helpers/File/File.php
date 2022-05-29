3f7224adc69ea685ade5e2ffc00ab769b9df46d7
Alexandre Gérault <gerault-alexandre@orange.fr>
2022-05-29 12:28:48 +0200

    test: add a file helper (copied from Laravel source code)

---
+++ apps/symfony/tests/Helpers/File/File.php
@@ -0,0 +1,74 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Helpers\File;
+
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
+class File extends UploadedFile
+{
+    public ?string $name = null;
+
+    /** @var resource */
+    public $tempFile;
+
+    public ?int $sizeToReport = null;
+
+    public ?string $mimeTypeToReport = null;
+
+    /**
+     * @param string $name
+     * @param resource $tempFile
+     */
+    public function __construct(string $name, $tempFile) {
+        $this->name = $name;
+        $this->tempFile = $tempFile;
+
+        parent::__construct($this->tempFilePath(), $name, $this->getMimeType(),null, true);
+    }
+
+    public static function create(string $name, string|int $kilobytes = 0): static
+    {
+        return (new FileFactory())->create($name, $kilobytes);
+    }
+
+    public static function createWithContent(string $name, string $content): static
+    {
+        return (new FileFactory())->createWithContent($name, $content);
+    }
+
+    public static function image(string $name, int $width = 10, int $height = 10): static
+    {
+        return (new FileFactory())->image($name, $width, $height);
+    }
+
+    public function size(int $kilobytes = 0): static
+    {
+        $this->sizeToReport = $kilobytes * 1024;
+
+        return $this;
+    }
+
+    public function getSize(): int
+    {
+        return $this->sizeToReport ?: parent::getSize();
+    }
+
+    public function mimeType(string $mimeType): static
+    {
+        $this->mimeTypeToReport = $mimeType;
+
+        return $this;
+    }
+
+    public function getMimeType(): string
+    {
+        return $this->mimeTypeToReport ?: MimeType::from($this->name);
+    }
+
+    protected function tempFilePath()
+    {
+        return stream_get_meta_data($this->tempFile)['uri'];
+    }
+}
---
+++ apps/symfony/tests/Helpers/File/MimeType.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Helpers\File;
+
+use Symfony\Component\Mime\MimeTypes;
+use Symfony\Component\Mime\MimeTypesInterface;
+
+class MimeType
+{
+    private static ?MimeTypes $mime = null;
+
+    public static function getMimeTypes(): MimeTypes|MimeTypesInterface|null
+    {
+        if (self::$mime === null) {
+            self::$mime = new MimeTypes;
+        }
+
+        return self::$mime;
+    }
+
+    public static function from(string $filename): string
+    {
+        $extension = pathinfo($filename, PATHINFO_EXTENSION);
+
+        return self::get($extension);
+    }
+
+    public static function get(string $extension): string
+    {
+        return self::getMimeTypes()->getExtensions($extension)[0] ?? 'application/octet-stream';
+    }
+
+    public static function search(string $mimeType): ?string
+    {
+        return self::getMimeTypes()->getExtensions($mimeType)[0] ?? null;
+    }
+}
