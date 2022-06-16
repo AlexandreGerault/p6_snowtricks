0a6919da457f9edec75d5d374003e8c35611113f
Alexandre Gérault <gerault-alexandre@orange.fr>
2022-06-16 04:42:21 +0200

    feat(trick): handle file changes and save aggregate

--- apps/symfony/tests/Domain/Trick/Adapters/InMemoryImageStorage.php
+++ apps/symfony/tests/Domain/Trick/Adapters/InMemoryImageStorage.php
@@ -5,10 +5,13 @@ declare(strict_types=1);
 namespace App\Tests\Domain\Trick\Adapters;
 
 use App\Trick\Core\ImageStorage;
+use PHPUnit\Framework\Assert;
 
 class InMemoryImageStorage implements ImageStorage
 {
-    private array $images = [];
+    public function __construct(private array $images = [])
+    {
+    }
 
     public function findAll(): array
     {
@@ -20,5 +23,22 @@ class InMemoryImageStorage implements ImageStorage
         $this->images[] = $path;
 
         return $path;
+    }
+
+    public function assertImageExists(string $path): void
+    {
+        Assert::assertContains($path, $this->images);
+    }
+
+    public function assertImageDoesNotExist(string $path): void
+    {
+        Assert::assertNotContains($path, $this->images);
+    }
+
+    public function delete($path): void
+    {
+        $this->images = array_filter($this->images, function ($image) use ($path) {
+            return $image !== $path;
+        });
     }
 }
