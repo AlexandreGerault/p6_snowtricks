0e6ee1dd2010b29627f220663aea91dec6e2194b
Alexandre Gérault <gerault-alexandre@orange.fr>
2022-06-16 04:45:28 +0200

    fix(trick): add one image and one video in trick fixtures

--- apps/symfony/src/Trick/Infrastructure/DataFixtures/TrickFixture.php
+++ apps/symfony/src/Trick/Infrastructure/DataFixtures/TrickFixture.php
@@ -4,15 +4,21 @@ declare(strict_types=1);
 
 namespace App\Trick\Infrastructure\DataFixtures;
 
+use App\Trick\Core\Image;
+use App\Trick\Core\ImageStorage;
+use App\Trick\Core\Video;
 use App\Trick\Infrastructure\Entity\Category;
 use App\Trick\Infrastructure\Entity\Trick;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Symfony\Component\Uid\Uuid;
-use Symfony\Component\Uid\UuidV6;
 
 class TrickFixture extends Fixture
 {
+    public function __construct(private readonly ImageStorage $imageStorage)
+    {
+    }
+
     public function load(ObjectManager $manager)
     {
         /** @var Category $category */
@@ -25,8 +31,20 @@ class TrickFixture extends Fixture
         $trick->setDescription('Description 1');
         $trick->setCategory($category);
 
+        $image = $this->copyAssetImageForTrick($trick, '180-1.jpg');
+        $trick->addImage($image);
+
+        $trick->addVideo(new Video('https://www.youtube.com/watch?v=dQw4w9WgXcQ'));
+
         $manager->persist($trick);
 
         $manager->flush();
+    }
+
+    private function copyAssetImageForTrick(Trick $trick, string $imageName): Image
+    {
+        $path = $this->imageStorage->save(__DIR__ . '/../../../../assets/fixtures/tricks/' . $imageName);
+
+        return new Image($path, $imageName);
     }
 }
